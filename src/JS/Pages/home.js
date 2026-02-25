0a7e34e53bda49cefc6889201607404083b8bb10
Octalink <btfoctalink@gmail.com>
2026-02-25 22:51:01 +0100

    grid style and dynamic load

--- src/JS/Pages/home.js
+++ src/JS/Pages/home.js
@@ -1,6 +1,6 @@
 
 
-const btn = document.getElementById("game-grid")
+const grid = document.getElementById("game-grid")
 
 async function getAllGames() {
     const reponse = await fetch("/DB/games/get/all");
@@ -13,13 +13,52 @@ document.addEventListener("DOMContentLoaded", async function () {
     
     const games = await getAllGames();
 
-    for (const element of games) {
-        console.log(element.name)
+    for (const game of games) {
+        grid.innerHTML += createCard(game)
     }
 });
 
+function createCard(game){
+    const img_path = game.img_path.replace("./res", "")
+    const rating = game.rating
+    const stars = getStars(rating)
+    const playtime = game.playtime
 
-btn.addEventListener('click', async function () {
-    btn.textContent =" wasd"
-    
-});
+
+    return `<div class="game-card">
+        <img src="${img_path}">
+        <div class="game-context">
+          <span class="hours">${playtime} h</span>
+          <div class="game-rating">
+            <span class="stars">${stars}</span>
+            <span class="rate"><span>${rating}</span>/10</span>
+          </div>
+          <div class="tags-list">
+            <span class="tag">Souls</span>
+          </div>
+        </div>
+      </div>`
+}
+
+function getStars(rating) {
+    let nbrEtoiles = Math.floor(rating / 2);
+    console.log(nbrEtoiles)
+
+    let reste = rating % 2;
+
+    let stars = "★".repeat(nbrEtoiles);
+
+    if (reste !== 0) {
+        stars += "⯪";
+    }
+
+    if (5-nbrEtoiles >= 1) {
+        var offset = 0
+        if (reste !== 0) {
+            offset = 1
+        }
+        stars += "☆".repeat(5-nbrEtoiles-offset)
+    }    
+
+    return stars
+}
