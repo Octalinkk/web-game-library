46e186432b51bf6f51b48b91772172b225b9cfe1
Octalink <btfoctalink@gmail.com>
2026-06-16 12:02:30 +0200

    WSIP done

--- src/JS/Pages/wsip.js
+++ src/JS/Pages/wsip.js
@@ -11,8 +11,10 @@ const btnCalc = document.getElementById("calc")
 
 const loading = document.getElementById("loading")
 const loader = document.getElementById("loader")
+const title = document.getElementById("title")
 const formDiv = document.getElementById("form")
 const resDiv = document.getElementById("result")
+const grid = document.getElementById("game-grid")
 
 async function getAllGamesID() {
     const reponse = await fetch("/DB/games/get/all/ID");
@@ -28,6 +30,30 @@ async function getGameById(game_id) {
 
 async function getGamesIdByTagId(tag_id) {
     const reponse = await fetch("/DB/game-tagID/"+tag_id);
+    const tags = await reponse.json();
+    return tags
+}
+
+async function getSupportbyID(supportId) {
+    const reponse = await fetch("/DB/supports/"+supportId);
+    const support = await reponse.json();
+    return support
+}
+
+async function getSupportsbyGameID(game_id) {
+    const reponse = await fetch("/DB/support-gameID/"+game_id);
+    const supports = await reponse.json();
+    return supports
+}
+
+async function getTagById(tag_id) {
+    const reponse = await fetch("/DB/tags/"+tag_id);
+    const support = await reponse.json();
+    return support
+}
+
+async function getTagsbyGameID(game_id) {
+    const reponse = await fetch("/DB/tag-gameID/"+game_id);
     const tags = await reponse.json();
     return tags
 }
@@ -165,7 +191,8 @@ btnCalc.addEventListener("click", async function(event) {
     
     loading.innerText = "DONE !"
     loader.classList.add("hidden");
-    console.log(chosen_games)
+    title.classList.remove("hidden")
+    displayGames(chosen_games);
 });
 
 
@@ -175,4 +202,103 @@ function concatArray(arr1, arr2) {
             arr1.push(item.game_id)
         }
     }
+}
+
+async function displayGames(games){
+
+    grid.innerHTML = ""
+    for (const g of games) {
+        const game = g.game       
+        const supportsId = await getSupportsbyGameID(game.id);
+        const tagsId = await getTagsbyGameID(game.id);
+        let supports = []
+        for (const elem of supportsId) {
+            const support = await getSupportbyID(elem.support_id)
+            supports.push(support)
+        }
+        let tags = []
+        for (const elem of tagsId) {
+            const tag = await getTagById(elem.tag_id)
+            tags.push(tag)
+        }
+        grid.innerHTML += createCard(game, supports, tags)
+    }
+}
+
+function createCard(game, supports, tags){
+
+    const img_path = game.img_path.replace("./res", "")
+    const rating = game.rating
+    const stars = getStars(rating)
+    const playtime = game.playtime
+
+    let card = `<div class="game-card">
+        <img class="card-bg" src="${img_path}">
+        <div class="game-context">
+          <div class="game-info">
+            <div class="hours">${playtime} h</div>
+            <div class="supports">`
+            for (const support of supports) {
+                const match = support.icon_path.replace("./res", "").replace("/icon", "").replace(".png", "")
+                switch (match){
+                    case "/3ds": {card += `<div class="icon ds"><img src="/icon/3ds.png"></div>`; break;}
+                    case "/epic": {card += `<div class="icon epic"><img src="/icon/epic.png"></div>`; break;}
+                    case "/ps": {card += `<div class="icon ps"><img src="/icon/ps.png"></div>`; break;}
+                    case "/steam": {card += `<div class="icon steam"><img src="/icon/steam.png"></div>`; break;}
+                    case "/switch": {card += `<div class="icon switch"><img src="/icon/switch.png"></div>`; break;}
+                    case "/wii": {card += `<div class="icon wii"><img src="/icon/wii.png"></div>`; break;}
+                }                
+            }
+            card += `</div>
+          </div>
+          <div class="game-rating">`
+            if (game.playtime > 0) {
+                card += `<span class="stars">${stars}</span>
+                        <span class="rate"><span>${rating}</span>/10</span>`
+            }
+            else {
+                card += `<span class="np">Never Played</span>`
+            }
+            
+        card += `
+          </div>
+          <div class="tags-list">`
+          for (const tag of tags) {
+            card += `<span class="tag">${tag.name}</span>`
+          }            
+          card += `</div>
+          <div class="labels">`
+            if(tags.some(tag => tag.name === 'Coup de coeur')) {
+                card += `<div class="label"><img src="/icon/heart.png"></div>`
+            }
+            if(tags.some(tag => tag.name === '100%')) {
+                card += `<div class="label"><img src="/icon/completion.png"></div>`
+            }
+            card += `</div>
+        </div>
+      </div>`
+
+    return card
+}
+
+function getStars(rating) {
+    let nbrEtoiles = Math.floor(rating / 2);
+
+    let reste = rating % 2;
+
+    let stars = "★".repeat(nbrEtoiles);
+
+    if (reste !== 0) {
+        stars += "⯪";
+    }
+
+    if (5-nbrEtoiles >= 1) {
+        var offset = 0
+        if (reste !== 0) {
+            offset = 1
+        }
+        stars += "☆".repeat(5-nbrEtoiles-offset)
+    }    
+
+    return stars
 }
